a3358f2697b22bfc73f356c8553b187566baa3e2
Nitin21nik <nrawat636@gmail.com>
2021-12-26 20:29:44 +0530

    adding users routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,9 @@ const router=express.Router();
 const homeController=require('../controllers/home_controller');
 console.log('router loaded');
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
+//for any further routes,access from here
+//router.user('/routerName',require('./routerFile'));
+
 module.exports=router;
