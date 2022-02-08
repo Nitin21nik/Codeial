c65049abd6bfffc965c0a275999316f476a7f67e
Nitin21nik <96187297+Nitin21nik@users.noreply.github.com>
2022-02-08 15:59:31 +0530

    add posts to the database

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const homeController=require('../controllers/home_controller');
 console.log('router loaded');
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
+router.use('/posts',require('./post'));
 
 //for any further routes,access from here
 //router.user('/routerName',require('./routerFile'));
