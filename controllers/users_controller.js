9b6a9f58a67808971264109dde02382d8d3c8c42
Nitin21nik <96187297+Nitin21nik@users.noreply.github.com>
2022-03-17 12:32:18 +0530

    added file upload functionality

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,17 +1,19 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
 module.exports.profile = function (req, res) {
     User.findById(req.params.id, function (err, user) {
         return res.render('user_profile', {
             title: "User Profile",
-            profile_user:user
+            profile_user: user
         });
     });
 }
 
 
-module.exports.update=function(req,res){
-    if(req.user.id == req.params.id)
+module.exports.update = async function (req, res) {
+    /*if(req.user.id == req.params.id)
     {
         User.findByIdAndUpdate(req.params.id,req.body,function(err,user)
         {
@@ -19,8 +21,41 @@ module.exports.update=function(req,res){
         });
     }
     else{
+        req.flash('error','Unauthorized');
        return res.status(401).send('Unauthorized');
+    }*/
+
+    if (req.user.id == req.params.id) {
+        try {
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function (err) {
+                if (err) { console.log('****Multer Error:', err) }
+
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if (req.file) {
+
+                    if (user.avatar) {
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
+                    //this is to save the path of the uploaded file in the avatar field of user
+                    user.avatar = User.avatarPath + '/' + req.file.filename
+                }
+
+                user.save();
+                return res.redirect('back');
+            });
+        } catch (err) {
+            req.flash('error', err);
+            return res.redirect('back');
+        }
     }
+    else {
+        req.flash('error', 'Unauthorized');
+        return res.status(401).send('Unauthorized');
+    }
+
 }
 
 //render the sign-up page
@@ -75,12 +110,12 @@ module.exports.create = function (req, res) {
 
 //sign in and create the session for the user
 module.exports.createSession = function (req, res) {
-    req.flash('success','Logged in Successfully');
+    req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function (req, res) {
     req.logout();
-    req.flash('success','You have logged out');
+    req.flash('success', 'You have logged out');
     return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -26,6 +26,8 @@ app.use(cookieParser());
 
 app.use(express.static('./assets'));
 
+app.use('/uploads',express.static(__dirname+'/uploads'));
+
 app.use(expressLayouts);
 
 //extract style and scripts from the sub pages into the layout
