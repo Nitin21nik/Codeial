5cc18cd588f0629fcd713d5a9e3d30c133987e2a
Nitin21nik <96187297+Nitin21nik@users.noreply.github.com>
2022-03-20 13:54:46 +0530

    socialauthentication

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,45 @@
+const passport=require('passport');
+const googleStrategy=require('passport-google-oauth').OAuth2Strategy;
+const crypto=require('crypto');
+const User=require('../models/user');
+
+//tell passport to use a new strategy for google login
+passport.use(new googleStrategy({
+        clientID:"411612928628-uud5n4ml3lndm9ds31hjp43c48rpveju.apps.googleusercontent.com",
+        clientSecret:"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",
+        callbackURL:"http://localhost:8000/users/auth/google/callback"
+     },
+
+     function(accessToken,refreshToken,profile,done){
+         //find a user
+         User.findOne({email:profile.emails[0].value}).exec(function(err,user){
+             if(err){
+                 console.log('error in google strategy-passport',err);
+                 return;
+             }
+             console.log(accessToken,refreshToken);
+             console.log(profile);
+
+             if(user){
+                 //if found,set this user as req.user
+                 return done(null,user);
+             }else{
+                 //if not found,create the user and set it as req.user
+                 User.create({
+                     name:profile.displayName,
+                     email:profile.emails[0],
+                     password:crypto.randomBytes(20).toString('hex')
+                 },function(err,user){
+                    if(err){
+                        console.log('error in creating user google strategy-passport',err);
+                        return;
+                    }
+                    return done(null,user);
+                 });
+             }
+         });
+     }
+));
+
+
+module.exports=passport;
--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,21 +1,21 @@
-const Post=require('../../../models/post');
-const Comment=require('../../../models/comment');
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
 
-module.exports.index=async function(req,res){
+module.exports.index = async function (req, res) {
 
     let posts = await Post.find({})
-         .sort('-createdAt')
-         .populate('user')
-         .populate({
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
             path: 'comments',
             populate: {
-               path: 'user'
+                path: 'user'
             }
-         });
+        });
 
-    return res.json(200,{
-        message:"List of posts",
-        posts:posts
+    return res.json(200, {
+        message: "List of posts",
+        posts: posts
     })
 }
 
@@ -24,25 +24,26 @@ module.exports.destroy = async function (req, res,) {
     try {
         let post = await Post.findById(req.params.id);
         //.id means converting the object id into string
-       // if (post.user == req.user.id) {
+        if (post.user == req.user.id) {
             post.remove();
 
             await Comment.deleteMany({ post: req.params.id });
 
-           // req.flash('success','Post and associated comments deleted!');
+            // req.flash('success','Post and associated comments deleted!');
 
-            return res.json(200,{
-                message:"Posts and associated comments deleted successfully"
+            return res.json(200, {
+                message: "Posts and associated comments deleted successfully"
             });
-        
-        // else {
-        //     req.flash('error','You cannot delete this post!');
-        //     return res.redirect('back');
-        // }
+        }
+        else {
+            return res.json(401, {
+                message: "You cannot delete this post!"
+            })
+        }
     } catch (err) {
         //req.flash('error',err);
-        return res.json(500,{
-            message:"Internal Server Error"
+        return res.json(500, {
+            message: "Internal Server Error"
         });
     }
 }
