9a916dca723f1942527f0bba7229081cb0661523
Nitin21nik <96187297+Nitin21nik@users.noreply.github.com>
2022-03-22 20:24:29 +0530

    setting up nodemailer functionality

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,65 +1,79 @@
-const Post = require('../models/post');
 const Comment = require('../models/comment');
+const Post = require('../models/post');
+const commentsMailer = require('../mailers/comments_mailer');
+module.exports.create = async function(req, res){
 
-module.exports.create = async function(req, res) {
-    try {
+    try{
         let post = await Post.findById(req.body.post);
-        if (post) {
-            let comment=await Comment.create(
-                {
-                    content: req.body.content,
-                    post: req.body.post,
-                    user: req.user._id
-                });
 
-            //adding comment to the post using mongodb feature
+        if (post){
+            let comment = await Comment.create({
+                content: req.body.content,
+                post: req.body.post,
+                user: req.user._id
+            });
+
             post.comments.push(comment);
             post.save();
             
+            comment = await comment.populate('user', 'name email');
+            commentsMailer.newComment(comment);
             if (req.xhr){
                 return res.status(200).json({
                     data: {
                         comment: comment
                     },
-                    message: "Comment created!"
+                    message: "Post created!"
                 });
             }
 
-            req.flash('success','Comment Published!!')
+
+            req.flash('success', 'Comment published!');
+
             res.redirect('/');
         }
-    } catch (err) {
-        console.log('Error', err);
+    }catch(err){
+        req.flash('error', err);
         return;
     }
+    
 }
 
-module.exports.destroy =async function (req, res) {
-    try {
+
+module.exports.destroy = async function(req, res){
+
+    try{
         let comment = await Comment.findById(req.params.id);
-        if (comment.user == req.user.id) {
+
+        if (comment.user == req.user.id){
+
             let postId = comment.post;
 
-            await comment.remove();
-            await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id}});
+            comment.remove();
 
-            if(req.xhr){
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
                 return res.status(200).json({
-                    data:{
-                        comment_id:req.params.id
+                    data: {
+                        comment_id: req.params.id
                     },
-                    message:'Comment Deleted!!!'
+                    message: "Post deleted"
                 });
             }
 
-            req.flash('success','Comment Deleted!!');
+
+            req.flash('success', 'Comment deleted!');
+
+            return res.redirect('back');
+        }else{
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
-        else {
-            return res.redirect('back');
-        }
-    } catch (err) {
-        console.log('Error', err);
+    }catch(err){
+        req.flash('error', err);
         return;
     }
+    
 }
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,16 +2,6 @@ const Post = require('../models/post');
 const User = require('../models/user');
 
 module.exports.home = async function (req, res) {
-   /*console.log(req.cookies);
-   res.cookie('user_id',25);
-   Post.find({},function(err,posts)
-     {
-    return res.render('home',{
-     title:"Codeial | Home",
-      posts: posts
-      });
-   });*/
-
    try {
       //populate the user of each post
       let posts = await Post.find({})
